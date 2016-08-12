2e36854f2b40260039429a7a4ea3aeb3f8dc9e11
klozz <klozz707@gmail.com>
2016-08-12 05:52:29 -0500

    a5ul: Fix libinit_msm removal
    Change-Id: I90a48d15ed8b3063bd5489e4bd41e89a6fc45628
    Signed-off-by: klozz <klozz707@gmail.com>

--- init/init_a5ul.cpp
+++ init/init_a5ul.cpp
@@ -61,7 +61,7 @@ void gsm_properties(char const default_network[])
     property_set("ro.telephony.ril_class", "A5RIL");
 }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
     char bootmid[PROP_VALUE_MAX];
